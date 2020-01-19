386ff756fa5256110f033bad944126fc10d3e9e0
Marcelo Reichert <marcelo@powertask.com.br>
2020-01-19 11:26:41 -0300

    Fix encode para Jason

--- lib/olivia/chat/interface/fb_messenger/dispatcher.ex
+++ lib/olivia/chat/interface/fb_messenger/dispatcher.ex
@@ -15,7 +15,7 @@ defmodule Olivia.Chat.Interface.FbMessenger.Dispatcher do
   end
 
   def send_response(response) do
-    url = "https://graph.facebook.com/v2.6/me/messages?access_token=#{@page_access_token}"
+    url = "https://graph.facebook.com/v2.11/me/messages?access_token=#{@page_access_token}"
 
     case HTTPoison.post(url, Jason.encode!(response), @headers) do
       {:ok, %HTTPoison.Response{status_code: 200}} ->
@@ -30,7 +30,7 @@ defmodule Olivia.Chat.Interface.FbMessenger.Dispatcher do
   def send_response(response, token) do
     url = "https://graph.facebook.com/v2.6/me/messages?access_token=#{token}"
 
-    case HTTPoison.post(url, Poison.encode!(response), @headers) do
+    case HTTPoison.post(url, Jason.encode!(response), @headers) do
       {:ok, %HTTPoison.Response{status_code: 200}} ->
         Conversation.sent_message(response["recipient"]["id"], response)
         :ok
