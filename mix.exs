6eb1bf23346a9981bfc058159b11ad99e366254c
Marcelo Reichert <marcelo@powertask.com.br>
2023-02-09 14:17:24 -0300

    update dependencies

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Olivia.Mixfile do
   def project do
     [
       app: :olivia,
-      version: "0.0.1",
-      elixir: "~> 1.9",
+      version: "0.2.0",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -49,22 +49,22 @@ defmodule Olivia.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.7"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.0"},
+      {:phoenix, "~> 1.6.15"},
+      {:phoenix_pubsub, "~> 2.1"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.9"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:httpoison, "~> 1.6"},
-      {:timex, "~> 3.5"},
-      {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.8", only: :test},
-      {:ex_doc, "~> 0.19", only: :dev},
-      {:accessible, "~> 0.2.1"}
+      {:phoenix_html, "~> 3.2"},
+      {:phoenix_live_reload, "~> 1.4", only: :dev},
+      {:gettext, "~> 0.22"},
+      {:jason, "~> 1.4"},
+      {:plug_cowboy, "~> 2.6"},
+      {:httpoison, "~> 2.0"},
+      {:timex, "~> 3.7"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.15.3", only: :test},
+      {:ex_doc, "~> 0.29", only: :dev},
+      {:accessible, "~> 0.3.0"}
     ]
   end
 
